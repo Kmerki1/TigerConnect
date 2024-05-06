b2019afde9bd16beec64f39c79e536af33809fbf
erichm3 <egr1365@gmail.com>
2024-05-06 10:57:34 -0400

    edited css for home page

--- Frontend/src/components/home/Home.jsx
+++ Frontend/src/components/home/Home.jsx
@@ -1,66 +1,75 @@
-import React from 'react';
-import "../Post.jsx"
+import React from "react";
+import "../Post.jsx";
 import { Post } from "../Post.jsx";
-import "../../styles/home.css"
+import "../../styles/home.css";
 import Separator from "../Separator.jsx";
 
 function Home() {
-    const postsData = [
-        {
-            "id": 1,
-            "author": "Jackson Patel",
-            "username": "IdeaJunkie87",
-            "content": "Just had a thought provoking conversation that left be buzzing with ideas!",
-            "time": "4/9/2024 4:28pm",
-            "likes": 10,
-        },
-        {
-            "id": 2,
-            "author": "Isabella Lopez",
-            "username": "CozyCorner123",
-            "content": "Weekend plans: Netflix, cozy blanket, and a big mug of tea.",
-            "time": "4/10/2024 7:17am",
-            "likes": 7,
-        },
-        {
-            "id": 3,
-            "author": "Noah Thompson",
-            "username": "BookWormExplorer",
-            "content": "Sometimes the best adventures are found in the pages of a book. What's your current literary escape?",
-            "time": "4/11/2024 2:09am",
-            "likes": 12,
-        },
-        {
-            "id": 4,
-            "author": "Ava Carter",
-            "username": "ZenSpirit21",
-            "content": "Today's mantra: Embrace the chaos and find beauty in the mess.",
-            "time": "4/12/2024 1:47pm",
-            "likes": 23,
-        },
-    ];
+  const postsData = [
+    {
+      id: 1,
+      author: "Jackson Patel",
+      username: "IdeaJunkie87",
+      content:
+        "Just had a thought provoking conversation that left be buzzing with ideas!",
+      time: "4/9/2024 4:28pm",
+      likes: 10,
+    },
+    {
+      id: 2,
+      author: "Isabella Lopez",
+      username: "CozyCorner123",
+      content: "Weekend plans: Netflix, cozy blanket, and a big mug of tea.",
+      time: "4/10/2024 7:17am",
+      likes: 7,
+    },
+    {
+      id: 3,
+      author: "Noah Thompson",
+      username: "BookWormExplorer",
+      content:
+        "Sometimes the best adventures are found in the pages of a book. What's your current literary escape?",
+      time: "4/11/2024 2:09am",
+      likes: 12,
+    },
+    {
+      id: 4,
+      author: "Ava Carter",
+      username: "ZenSpirit21",
+      content: "Today's mantra: Embrace the chaos and find beauty in the mess.",
+      time: "4/12/2024 1:47pm",
+      likes: 23,
+    },
+  ];
 
-
-    return (
-        <div>
-            <div id="posts" className="post-section">
-                {postsData.map((post, index) => (
-                    <div key={post.id}>
-                        <Post
-                            id={post.id}
-                            name={post.author}
-                            tag={post.username}
-                            content={post.content}
-                            date={post.time}
-                            likes={post.likes}
-                        />
-                        {index < postsData.length - 1 && <Separator/>}
-                    </div>
-                ))}
-
-            </div>
+  return (
+    <div id="home-container">
+      <div id="posts-container">
+        <div id="create-post">
+          <input type="text" placeholder="What should I post?" />
+          <button id="post-button">Post</button>
         </div>
-    );
+        <div id="posts" className="post-section">
+          {postsData.map((post, index) => (
+            <div key={post.id}>
+              <Post
+                id={post.id}
+                name={post.author}
+                tag={post.username}
+                content={post.content}
+                date={post.time}
+                likes={post.likes}
+              />
+              {index < postsData.length - 1 && <Separator />}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div id="sidebar">
+        <h3>Sidebar</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
